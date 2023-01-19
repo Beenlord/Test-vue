471c901b404fdd35e6374de94d85935c1eda945b
Beenlord <beenlord@gmail.com>
2023-01-19 23:04:27 +0300

    Initial commit

---
+++ application/Application.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="Application">
+        <Resume />
+    </div>
+</template>
+
+<script>
+import Resume from './assets/components/Resume';
+
+export default {
+    components: {
+        Resume,
+    },
+    data() {
+        return {};
+    },
+};
+</script>
+
+<style lang="scss">
+.Application {}
+</style>
